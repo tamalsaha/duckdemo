2e2084c7f16fe5f73a9a2c74d598b5c517cd93eb
Tamal Saha <tamal@appscode.com>
2022-10-05 17:16:17 -0700

    added-all
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- pod-client/main.go
+++ pod-client/main.go
@@ -1,5 +1,63 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
 
+	apps "k8s.io/api/apps/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/klog/v2/klogr"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/apiutil"
+)
+
+func NewClient() (client.Client, error) {
+	scheme := runtime.NewScheme()
+	_ = clientgoscheme.AddToScheme(scheme)
+
+	ctrl.SetLogger(klogr.New())
+	cfg := ctrl.GetConfigOrDie()
+	cfg.QPS = 100
+	cfg.Burst = 100
+
+	mapper, err := apiutil.NewDynamicRESTMapper(cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	return client.New(cfg, client.Options{
+		Scheme: scheme,
+		Mapper: mapper,
+		//Opts: client.WarningHandlerOptions{
+		//	SuppressWarnings:   false,
+		//	AllowDuplicateLogs: false,
+		//},
+	})
+}
+
+func main() {
+	if err := useKubebuilderClient(); err != nil {
+		panic(err)
+	}
+}
+
+func useKubebuilderClient() error {
+	fmt.Println("Using kubebuilder client")
+	kc, err := NewClient()
+	if err != nil {
+		return err
+	}
+
+	var applist apps.DeploymentList
+	err = kc.List(context.TODO(), &applist)
+	if err != nil {
+		return err
+	}
+	fmt.Println()
+	for _, db := range applist.Items {
+		fmt.Println(client.ObjectKeyFromObject(&db))
+	}
+	return nil
 }
